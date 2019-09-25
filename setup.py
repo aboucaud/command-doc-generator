93fe652b64903a0a212782414c15084353686f92
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2019-09-25 19:41:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,23 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="ccsdoc",
-    version="0.0.3",
+    version="0.0.4",
     description="Parser tools for generating CCS Java documentation",
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Alexandre Boucaud",
     author_email="aboucaud@apc.in2p3.fr",
     packages=find_packages(),
     license="BSD",
-    python_requires='>=3.6',
-    install_requires = [
-        "click",
-        "pandas",
-    ],
-    entry_points = {
-        "console_scripts": [
-            "ccsdoc = ccsdoc.scripts.cli:cli",
-        ],
-    },
+    python_requires=">=3.6",
+    install_requires=["click", "pandas"],
+    entry_points={"console_scripts": ["ccsdoc = ccsdoc.scripts.cli:cli"]},
     zip_safe=False,
 )
