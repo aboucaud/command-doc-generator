cef2b1406507eb052f61dbde63ca673eb54963e8
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2019-09-27 01:29:11 +0200

    Add argument class

---
+++ ccsdoc/argument.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from ccsdoc.text import clean_description
+
+
+class Argument:
+    def __init__(self, name: str, ptype: str, desc: Optional[str] = None):
+        self.name = name
+        self.type = ptype
+        self.description = clean_description(desc) if desc else ""
+
+    def __repr__(self):
+        return (
+            f"{self.type} {self.name}"
+        )
+
+    def __str__(self):
+        return (
+            f"{self.__class__.__name__}("
+            f"name={self.name}"
+            f", type={self.type}"
+            f"{', desc=' + self.description if self.description else ''}"
+        )
